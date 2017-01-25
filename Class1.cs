a328181bbeb2296ff116cff63fa02675ed907dee
Zack <zdhickman@gmail.com>
2017-01-25 01:04:42 -0500

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace SprintLevelEditor
+{
+    public struct SimpleRectangle
+    {
+        public int X;
+        public int Y;
+        public int Width;
+        public int Height;
+        public Vector2 Position;
+
+        public SimpleRectangle(int x, int y, int width, int height, Vector2 position)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            Position = position;
+        }
+
+        public static SimpleRectangle fromWall(Wall wall)
+        {
+            Rectangle rectangle = wall.sprite.drawRect;
+            return new SimpleRectangle(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height, rectangle.Location.ToVector2());
+        }
+    }
+}
